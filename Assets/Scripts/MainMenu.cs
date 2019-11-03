1fb1e638b59cf6dce6d8a0b1448cdea5a2b222be
culve005 <44325484+culve005@users.noreply.github.com>
2019-11-03 09:18:22 -0800

    Working menu. No backdrop.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+    		Application.Quit();
+        #endif
+
+}
+}
