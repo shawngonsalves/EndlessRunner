2a523f6415a7c45cf54ce1ac18432d1170a1d99b
culve005 <44325484+culve005@users.noreply.github.com>
2019-11-04 21:29:47 -0800

    Prefab Effect Creaded
    Crate_13 is destroyed by clicking enter on keyboard. It is a prefab that can be added to any tile.
    
    Assets -> Prefabs -> Crate_13

---
+++ Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEngine.Events.UnityEvent crateToTrigger;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            crateToTrigger.Invoke();
+
+        }
+    }
+}
